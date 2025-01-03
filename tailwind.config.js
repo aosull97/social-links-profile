40a54fa54bcf5709bf5d950086678823b076d036
Alice <aosullivan@netpremacy.com>
2025-01-03 12:58:27 +0000

    Styles added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ["index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      'green': 'hsl(75, 94%, 57%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'grey-700': 'hsl(0, 0%, 20%)',
+      'grey-800': 'hsl(0, 0%, 12%)',
+      'grey-900': 'hsl(0, 0%, 8%)'
+    }
+
+
   },
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "serif"],
+      },
+    },
   plugins: [],
 }
-
